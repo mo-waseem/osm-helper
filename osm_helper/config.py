9771b75a6ebe0ce6f549010e0c3dabfd3d1ea8b8
Waseem Kntar <waseem.kntar@gmail.com>
2023-08-25 19:47:15 +0300

    fix config

--- osm_helper/config.py
+++ osm_helper/config.py
@@ -1,32 +1,40 @@
 import os
-from typing import Any
+import warnings
 import yaml
+
 from dotenv import load_dotenv
+from typing import Any
 
 load_dotenv()
 
 
 class Config:
     # You need to add this file to .gitignore of your project
-    with open(
-        os.getenv("CONFG_YAML_FILE_NAME", "osm_helper_config.yaml"), "r"
-    ) as stream:
-        try:
-            OSM_CONFIG = yaml.safe_load(stream)
-        except yaml.YAMLError as exc: # noqa
-            OSM_CONFIG = {}
-            pass
+    try:
+        with open(
+            os.getenv("CONFG_YAML_FILE_NAME", "osm_helper_config.yaml"), "r"
+        ) as stream:
+            try:
+                OSM_CONFIG = yaml.safe_load(stream) or {}
+            except yaml.YAMLError as exc: # noqa
+                OSM_CONFIG = {}
+    except FileNotFoundError:
+        OSM_CONFIG = {}
+    if not OSM_CONFIG:
+        warnings.warn("osm-helper: 'osm_helper_config.yaml' file is not setted.")
+        
 
-    __conf = OSM_CONFIG
+    __conf = OSM_CONFIG 
+    __default_osrm_url = "http://router.project-osrm.org/"
+    __yaml_osrm_url = __conf.get("PRIMARY_OSRM_URL", None) if __conf else None
+    __env_osrm_url = os.getenv("PRIMARY_OSRM_URL", None)
+    if not __env_osrm_url and not __yaml_osrm_url:
+        warnings.warn("osm-helper: 'PRIMARY_OSRM_URL' is not setted.")
+    __osrm_url = __env_osrm_url or __yaml_osrm_url or __default_osrm_url
+    
     __conf.update(
         {
-            "OSRM_URL": __conf.get(
-                "PRIMARY_OSRM_URL",
-                os.getenv(
-                    "PRIMARY_OSRM_URL",
-                    "http://router.project-osrm.org/",
-                ),
-            )
+            "OSRM_URL": __osrm_url
         }
     )
     __setters = ["OSRM_URL"]
